a46a989b32af7e0a423c8717927ae9027ba631fd
Maximiliano R Soria <maximilianorodrigosoria@gmail.com>
2019-08-01 16:23:09 -0300

    dasdas

--- src/main/java/ar/com/envio/email/example/controller/EmailController.java
+++ src/main/java/ar/com/envio/email/example/controller/EmailController.java
@@ -1,11 +1,9 @@
-package ar.com.envio.email.example;
+package ar.com.envio.email.example.controller;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,18 +12,20 @@ import ar.com.envio.email.example.dto.MailRequest;
 import ar.com.envio.email.example.dto.MailResponse;
 import ar.com.envio.email.example.service.EmailService;
 
-
-
-@SpringBootApplication
-public class EnvioDeEmailApplication {
-
-
+@RestController
+public class EmailController {
 
 	
-	public static void main(String[] args) {
-		SpringApplication.run(EnvioDeEmailApplication.class, args);
+	@Autowired
+	private EmailService service;
+	
+	@PostMapping("/enviarEmail")
+	public MailResponse sendEmail(@RequestBody MailRequest request) {
+		Map<String, Object> model = new HashMap<>();
+		model.put("Name", request.getName());
+		model.put("location", "Bangalore,India");
+		return service.sendEmail(request, model);
+
 	}
-
-	
 	
 }
